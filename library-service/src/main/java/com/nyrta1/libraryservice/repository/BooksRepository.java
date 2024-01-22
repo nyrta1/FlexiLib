fee72240619bb176317afe33e06559a0adbadeb3
nyrta1 <nurtai_2005@mail.ru>
2024-01-22 11:56:18 +0600

    refactor: rename to id to uuid

--- library-service/src/main/java/com/nyrta1/libraryservice/repository/BooksRepository.java
+++ library-service/src/main/java/com/nyrta1/libraryservice/repository/BooksRepository.java
@@ -3,8 +3,10 @@ package com.nyrta1.libraryservice.repository;
 import com.nyrta1.libraryservice.model.Books;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface BooksRepository
         extends JpaRepository<Books, UUID> {
+    Optional<Books> findByBookUUID(UUID uuid);
 }
