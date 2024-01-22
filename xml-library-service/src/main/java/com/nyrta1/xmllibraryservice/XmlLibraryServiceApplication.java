fd724d1808636d42b112df07bb46e520439cbcf9
nyrta1 <nurtai_2005@mail.ru>
2024-01-22 15:47:30 +0600

    feat: marked as discovery client

--- xml-library-service/src/main/java/com/nyrta1/xmllibraryservice/XmlLibraryServiceApplication.java
+++ xml-library-service/src/main/java/com/nyrta1/xmllibraryservice/XmlLibraryServiceApplication.java
@@ -2,8 +2,10 @@ package com.nyrta1.xmllibraryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class XmlLibraryServiceApplication {
 
 	public static void main(String[] args) {
