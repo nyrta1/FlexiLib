f905a4caea313a84487839469477b4988ec0ab3f
nyrta1 <nurtai_2005@mail.ru>
2024-01-22 10:07:13 +0600

    feat: added author service

---
+++ library-service/src/main/java/com/nyrta1/libraryservice/service/authorservice/AuthorService.java
@@ -0,0 +1,7 @@
+package com.nyrta1.libraryservice.service.authorservice;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface AuthorService {
+}
---
+++ library-service/src/main/java/com/nyrta1/libraryservice/service/authorservice/AuthorServiceImpl.java
@@ -0,0 +1,4 @@
+package com.nyrta1.libraryservice.service.authorservice;
+
+class AuthorServiceImpl implements AuthorService {
+}
