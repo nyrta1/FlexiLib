4684da9786ab7f677f09811b640ac4d19de466ab
nyrta1 <nurtai_2005@mail.ru>
2024-01-22 15:43:12 +0600

    build: created xml-library-service and added necessary dependencies

---
+++ xml-library-service/src/main/java/com/nyrta1/xmllibraryservice/XmlLibraryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nyrta1.xmllibraryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlLibraryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlLibraryServiceApplication.class, args);
+	}
+
+}
