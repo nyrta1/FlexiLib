7b36b60235ea2ac8d4f8108c3dec4b91452de2aa
nyrta1 <nurtai_2005@mail.ru>
2024-01-22 13:19:52 +0600

    feat: added get, post methods and ready to use

--- library-service/src/main/java/com/nyrta1/libraryservice/controller/AuthorsController.java
+++ library-service/src/main/java/com/nyrta1/libraryservice/controller/AuthorsController.java
@@ -1,10 +1,51 @@
 package com.nyrta1.libraryservice.controller;
 
+import com.nyrta1.libraryservice.model.Author;
+import com.nyrta1.libraryservice.service.authorservice.AuthorService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/api/authors")
 @Slf4j
 public class AuthorsController {
+    private final AuthorService authorService;
+
+    @Autowired
+    public AuthorsController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllAuthor() {
+        List<Author> authors = authorService.findAll();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(authors);
+    }
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<?> getAuthorByUUID(@Valid @PathVariable("uuid") UUID uuid) {
+        Author author = authorService.findByUUID(uuid);
+        return ResponseEntity.of(Optional.ofNullable(author));
+    }
+
+
+    @PostMapping
+    public ResponseEntity<UUID> saveAuthor(@RequestBody @Valid Author author) {
+        UUID authorUUID = authorService.save(author);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(authorUUID);
+    }
 
 }
--- library-service/src/main/java/com/nyrta1/libraryservice/controller/BooksController.java
+++ library-service/src/main/java/com/nyrta1/libraryservice/controller/BooksController.java
@@ -1,9 +1,51 @@
 package com.nyrta1.libraryservice.controller;
 
+import com.nyrta1.libraryservice.model.Book;
+import com.nyrta1.libraryservice.service.bookservice.BookService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/api/books")
 @Slf4j
 public class BooksController {
+    private final BookService bookService;
+
+    @Autowired
+    public BooksController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllBooks() {
+        List<Book> books = bookService.findAll();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(books);
+    }
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<?> getBookByUUID(@Valid @PathVariable("uuid") UUID uuid) {
+        Book book = bookService.findByUUID(uuid);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(book);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<UUID> saveAuthor(@RequestBody @Valid Book book) {
+        log.info(book.toString());
+        UUID bookUUID = bookService.save(book);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(bookUUID);
+    }
 }
