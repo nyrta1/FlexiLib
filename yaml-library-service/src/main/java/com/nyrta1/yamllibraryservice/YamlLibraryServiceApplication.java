8ff529c4e83e2bbf8c8043f01c731d0367dda337
nyrta1 <nurtai_2005@mail.ru>
2024-01-25 15:36:17 +0600

    refactor

--- yaml-library-service/src/main/java/com/nyrta1/yamllibraryservice/YamlLibraryServiceApplication.java
+++ yaml-library-service/src/main/java/com/nyrta1/yamllibraryservice/YamlLibraryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.nyrta1.xmllibraryservice;
+package com.nyrta1.yamllibraryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
